415b139894f928bd0a39c310ce32d06258c89d1a
Agata Ping <agataping2001@gmail.com>
2025-04-21 16:58:17 +0800

    total csr 21/04/2025

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -220,22 +220,26 @@ class ReportController extends Controller
 
         //csr
         $totactualscsr = (clone $query)
-        ->where(function ($q) {
-            $q->where('category_labarugis.namecategory', 'like', '%CSR%')
-              ->orWhere('category_labarugis.namecategory', 'like', '%PPM%')
-              ->orWhere('category_labarugis.namecategory', 'like', '%Sosial%');
-        })
-                    ->get()
+            // ->where(function ($q) {
+            //     $q->where('category_labarugis.namecategory', 'like', '%CSR%')
+            //         ->orWhere('category_labarugis.namecategory', 'like', '%PPM%')
+            //         ->orWhere('category_labarugis.namecategory', 'like', '%Sosial%');
+            // })
+            ->whereRaw("category_labarugis.namecategory COLLATE utf8mb4_general_ci LIKE ?", ['%CSR%'])
+
+            ->get()
             ->sum(function ($item) {
                 return (float)str_replace(',', '', $item->nominalactual ?? 0);
             });
         $totplanscsr = (clone $query)
-        ->where(function ($q) {
-            $q->where('category_labarugis.namecategory', 'like', '%CSR%')
-              ->orWhere('category_labarugis.namecategory', 'like', '%PPM%')
-              ->orWhere('category_labarugis.namecategory', 'like', '%Sosial%');
-        })
-                ->get()
+            // ->where(function ($q) {
+            //     $q->where('category_labarugis.namecategory', 'like', '%CSR%')
+            //         ->orWhere('category_labarugis.namecategory', 'like', '%PPM%')
+            //         ->orWhere('category_labarugis.namecategory', 'like', '%Sosial%');
+            // })
+            ->whereRaw("category_labarugis.namecategory COLLATE utf8mb4_general_ci LIKE ?", ['%CSR%'])
+
+            ->get()
             ->sum(function ($item) {
                 return (float)str_replace(',', '', $item->nominalplan ?? 0);
             });
